e1fec3b7009b9784bb7a089ec9d47f0a217fc5b3
hellokjy11 <72700837+hellokjy11@users.noreply.github.com>
2022-01-16 21:57:11 +0900

    Update axie_apis.py

--- axie_apis.py
+++ axie_apis.py
@@ -30,3 +30,26 @@ def get_axie_brief_list(from_, size, sort, auctionType):
   return r.text
 
   
+  
+  def get_recently_axies_sold(from_, size_):
+  '''
+  Params:
+  from_ : axie id to search from
+  size_ : how many axies to grep
+  '''
+  
+  payload ={
+    "operationName": "GetRecentlyAxiesSold",
+    "variables": {
+      "from" : from_,
+      "size" : size_
+    },
+  "query": "query GetRecentlyAxiesSold($from: Int, $size: Int) {\n  settledAuctions {\n    axies(from: $from, size: $size) {\n      total\n      results {\n        ...AxieSettledBrief\n        transferHistory {\n          ...TransferHistoryInSettledAuction\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieSettledBrief on Axie {\n  id\n  name\n  image\n  class\n  breedCount\n  __typename\n}\n\nfragment TransferHistoryInSettledAuction on TransferRecords {\n  total\n  results {\n    ...TransferRecordInSettledAuction\n    __typename\n  }\n  __typename\n}\n\nfragment TransferRecordInSettledAuction on TransferRecord {\n  from\n  to\n  txHash\n  timestamp\n  withPrice\n  withPriceUsd\n  fromProfile {\n    name\n    __typename\n  }\n  toProfile {\n    name\n    __typename\n  }\n  __typename\n}\n"
+  }
+
+
+  endpoint = "https://graphql-gateway.axieinfinity.com/graphql"
+
+  r = requests.post(endpoint, data= payload)
+
+  return r.text
