b15be07a852974661bcdc7b2f6c202a69d6c4cc5
rhanei <96103794+rhanei@users.noreply.github.com>
2022-01-16 20:50:03 +0800

    Update axie_apis.py

--- axie_apis.py
+++ axie_apis.py
@@ -1,32 +1,30 @@
 import requests
 
 
-def get_axie_brief_list(from_, size, sort, auctionType):
-  '''
-  Fetch axies with certain criterias
-  Params:
+def get_axie_transfer_history(axieId, from_, size):
+    '''
+    Fetches transfer history of an axie
+    Params:
+    axieId: axie ID
     from_ : axie id to search from
     size : how many axies to grep
-    sort : sorting criterias
-    auctionType : auction type
-  '''
+    '''
 
-  payload = {  
-  "operationName": "GetAxieBriefList",
-    "variables": {
-      "from": from_,
-      "size": size,
-      "sort": sort,
-      "auctionType": auctionType,
-      "criteria": {}
-    },
-    "query": "query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\n  axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {\n    total\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  id\n  name\n  stage\n  class\n  breedCount\n  image\n  title\n  battleInfo {\n    banned\n    __typename\n  }\n  auction {\n    currentPrice\n    currentPriceUSD\n    __typename\n  }\n  parts {\n    id\n    name\n    class\n    type\n    specialGenes\n    __typename\n  }\n  __typename\n}\n"
-  }
+    payload = {
+        "operationName": "GetAxieTransferHistory",
+         "variables": {
+            "axieId": axieId,
+            "from": from_,
+            "size": size
+  },
+  "query": "query GetAxieTransferHistory($axieId: ID!, $from: Int!, $size: Int!) {\n  axie(axieId: $axieId) {\n    id\n    transferHistory(from: $from, size: $size) {\n      ...TransferRecords\n      __typename\n    }\n    ethereumTransferHistory(from: $from, size: $size) {\n      ...TransferRecords\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment TransferRecords on TransferRecords {\n  total\n  results {\n    from\n    to\n    timestamp\n    txHash\n    withPrice\n    __typename\n  }\n  __typename\n}\n"
+}
 
-  endpoint = "https://graphql-gateway.axieinfinity.com/graphql"
+    endpoint = "https://graphql-gateway.axieinfinity.com/graphql"
+    r = requests.post(endpoint, data = payload)
 
-  r = requests.post(endpoint, data=payload)
-  
-  return r.text
+    return r.text
+func_get_axie_transfer_history = get_axie_transfer_history(5, 1, 1)
+print(func_get_axie_transfer_history)
 
   
